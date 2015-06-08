8d035ff7396b44926270d35328db01b3b163b738
imaginationcoder <labs@maisasolutions.com>
2015-06-08 19:26:17 +0530

    upvote/down vote on post

--- www/js/posts.js
+++ www/js/posts.js
@@ -21,16 +21,16 @@ function getPosts(){
             }
         },
         beforeSend: function () {
-          // show up spinner only if user visited first time
-          if(!localStorage.getItem('default_posts')){
-              $('.load-up-posts').show()
-          }
-          // when redirected here to after creating a post
-          if(sessionStorage['after-add-post']){
-              $('.load-up-posts').show()
-              // then remove
-              sessionStorage.removeItem('after-add-post')
-          }
+            // show up spinner only if user visited first time
+            if(!localStorage.getItem('default_posts')){
+                $('.load-up-posts').show()
+            }
+            // when redirected here to after creating a post
+            if(sessionStorage['after-add-post']){
+                $('.load-up-posts').show()
+                // then remove
+                sessionStorage.removeItem('after-add-post')
+            }
 
         },
         success: function (data) {
@@ -55,7 +55,7 @@ function getPosts(){
 function singlePostClick(){
     $('.single-post').on('click', function (e) {
         e.preventDefault()
-      //  alert($(this).data('uid'))
+        //  alert($(this).data('uid'))
         sessionStorage.setItem('post-uid', $(this).data('uid'))
         window.location.href = 'post-show.html'
     })
@@ -91,7 +91,9 @@ function singlePostReady(){
              alert(val);
              });
              */
+            upDownVotePost()
             commentReady() //invoke comment section for after
+
             hideAjaxSpinner()
         },
         error: function(xhr,textStatus,errorThrown ) {
@@ -122,7 +124,7 @@ function commentReady(){
                         'body': body_params
                     },
                     beforeSend: function () {
-                       showAjaxSpinner();
+                        showAjaxSpinner();
                     },
                     success: function (data) {
                         var source = $("#single-comment-template").html();
@@ -137,7 +139,7 @@ function commentReady(){
                         var error_obj = $.parseJSON(xhr.responseText)
                         console.log(error_obj)
                         errorDialog('Error', error_obj.message)
-                       hideAjaxSpinner();
+                        hideAjaxSpinner();
                     }
                 })
             }
@@ -146,4 +148,262 @@ function commentReady(){
             window.location.href = 'signin.html'
         }
     })
+
+
+    // set comment as user
+    if(localStorage['current_user']){
+        current_user = JSON.parse(localStorage.getItem('current_user'))
+        if(current_user.photo){
+            str = "<a href='#'>Comment as "+ current_user.fname +"<img src='"+ current_user.photo +"' alt=''> </a>"
+            $('.comment-as-user').html(str)
+        } else{
+            str ="<div class='profilepic-placeholder'>"
+            str += current_user.fname.substr(0, 1).toUpperCase() +""+current_user.lname.substr(0, 1).toUpperCase()
+            str ="</div>"
+            $('.comment-as-user').html(str)
+        }
+        $('.comment-as-ananymous').html('')
+
+    }
+
+    $('.comment-as-user').on('click',function(e){
+        current_user = JSON.parse(localStorage.getItem('current_user'))
+        if(current_user.photo){
+            var img = $('<img alt="">');
+            img.attr('src', current_user.photo);
+            $('#comment-as-picture').html(img)
+        }else{
+            name ="<div class='profilepic-placeholder'>"
+            name += current_user.fname.substr(0, 1).toUpperCase() +""+current_user.lname.substr(0, 1).toUpperCase()
+            name ="</div>"
+            $('#comment-as-picture').html(name)
+        }
+        str = "<a href='#'>Comment as anonymous <img alt='' src='img/ananymous-placeholder.png'> </a>"
+        $('.comment-as-ananymous').html(str)
+        $(this).html('')
+        $('#comment-form').find('input[name="anonymous"]').val(false);
+
+    })
+
+    $('.comment-as-ananymous').on('click',function(e){
+        current_user = JSON.parse(localStorage.getItem('current_user'))
+        var img = $('<img alt="" src="img/ananymous-placeholder.png">');
+        $('#comment-as-picture').html(img)
+        if(current_user.photo){
+            str = "<a href='#'>Comment as "+ current_user.fname +"<img src='"+ current_user.photo +"' alt=''> </a>"
+            $('.comment-as-user').html(str)
+        } else{
+            str ="<div class='profilepic-placeholder'>"
+            str += current_user.fname.substr(0, 1).toUpperCase() +""+current_user.lname.substr(0, 1).toUpperCase()
+            str ="</div>"
+            $('.comment-as-user').html(str)
+        }
+        $(this).html('')
+        $('#comment-form').find('input[name="anonymous"]').val(true);
+    })
+
+
+    // redirect to signin if current user present and clicks on comment as or navigation
+    $('#switch-comment-as').on('click',function(e){
+        if(!localStorage['current_user']){
+            window.location.href = 'signin.html'
+        }
+    })
+}
+
+function upDownVotePost(){
+    $('.up-down-vote-post').on('click',function(e){
+        e.preventDefault()
+        if (localStorage['current_user']) {
+        target = $(this)
+        command = target.hasClass('viewPostLikeActive') ? 'undoVoting' : 'upvote'
+        $.ajax({
+            url: window.api_url+'posts/'+$(this).data('id'),
+            type: 'post',
+            data: {
+                'command' : command,
+                'access_token':  localStorage['access_token'],
+            },
+            beforeSend: function () {
+                if(command == 'undoVoting'){
+                    target.removeClass('viewPostLikeActive')
+                    target.addClass('viewPostLike')
+                }else{
+                    target.removeClass('viewPostLike')
+                    target.addClass('viewPostLikeActive')
+                }
+                showAjaxSpinner();
+            },
+            success: function (data) {
+               if(command == 'undoVoting'){
+                   $("#post-upvote-count").text(parseInt($("#post-upvote-count").text()) - 1);
+               }else{
+                   $("#post-upvote-count").text(parseInt($("#post-upvote-count").text()) + 1);
+               }
+                hideAjaxSpinner();
+            },
+            error: function(xhr,textStatus,errorThrown ) {
+                var error_obj = $.parseJSON(xhr.responseText)
+                console.log(error_obj)
+                hideAjaxSpinner()
+                errorDialog('Error',error_obj.message)
+            }
+        })
+        }else{
+            window.location.href = 'signin.html'
+        }
+    })
+
+}
+
+
+
+
+function getFavoriteGroups(){
+    $.ajax({
+        url: window.api_url+'groups',
+        type: 'post',
+        data: {
+            'command' :"favourites",
+            'access_token':  localStorage['access_token'],
+        },
+        beforeSend: function () { showAjaxSpinner();  },
+        success: function (data) {
+            var source = $("#get-tags-template").html();
+            var template = Handlebars.compile(source);
+            // $('#comments').append(template(data.body));
+            $("#add-post-tags").html(template(data.body))//.hide().fadeIn();
+            hideAjaxSpinner()
+            addPostReady() // invoke add post ready
+        },
+        error: function(xhr,textStatus,errorThrown ) {
+            var error_obj = $.parseJSON(xhr.responseText)
+            console.log(error_obj)
+            hideAjaxSpinner()
+            errorDialog('Error',error_obj.message)
+        }
+    })
+}
+
+function addPostReady(){
+    $('#post-content-text').focus()
+    $("#addPost ul.tags-list li").click(function(e) {
+        e.stopPropagation();
+        tag =  $(this).find(".tagname")
+        if(tag.hasClass('tagname-active')){
+            tag.removeClass("tagname-active");
+            $(this).find('input').prop('checked',false);
+        }else{
+            tag.addClass("tagname-active");
+            $(this).find('input').prop('checked',true);
+        }
+    });
+
+    $("#addPost ul.tags-list li .css-label").click(function(e) {
+        $(this).parents('li:first').trigger( "click" );
+        return false;
+    });
+    if(localStorage['current_user']){
+        current_user = JSON.parse(localStorage.getItem('current_user'))
+        if(current_user.photo){
+            str = "<a href='#'>Post as "+ current_user.fname +"<img src='"+ current_user.photo +"' alt=''> </a>"
+            $('.post-as-user').html(str)
+        } else{
+            str ="<div class='profilepic-placeholder'>"
+            str += current_user.fname.substr(0, 1).toUpperCase() +""+current_user.lname.substr(0, 1).toUpperCase()
+            str ="</div>"
+            $('.post-as-user').html(str)
+        }
+        $('.post-as-ananymous').html('')
+
+    }
+    $('.post-as-user').on('click',function(e){
+        current_user = JSON.parse(localStorage.getItem('current_user'))
+        if(current_user.photo){
+            var img = $('<img alt="">');
+            img.attr('src', current_user.photo);
+            $('#post-as-picture').html(img)
+        }else{
+            name ="<div class='profilepic-placeholder'>"
+            name += current_user.fname.substr(0, 1).toUpperCase() +""+current_user.lname.substr(0, 1).toUpperCase()
+            name ="</div>"
+            $('#post-as-picture').html(name)
+        }
+        str = "<a href='#'>Post as anonymous <img alt='' src='img/ananymous-placeholder.png'> </a>"
+        $('.post-as-ananymous').html(str)
+        $(this).html('')
+        $('#add-post-form').find('input[name="anonymous"]').val(false);
+    })
+
+    $('.post-as-ananymous').on('click',function(e){
+        current_user = JSON.parse(localStorage.getItem('current_user'))
+        var img = $('<img alt="" src="img/ananymous-placeholder.png">');
+        $('#post-as-picture').html(img)
+        if(current_user.photo){
+            str = "<a href='#'>Post as "+ current_user.fname +"<img src='"+ current_user.photo +"' alt=''> </a>"
+            $('.post-as-user').html(str)
+        } else{
+            str ="<div class='profilepic-placeholder'>"
+            str += current_user.fname.substr(0, 1).toUpperCase() +""+current_user.lname.substr(0, 1).toUpperCase()
+            str ="</div>"
+            $('.post-as-user').html(str)
+        }
+        $(this).html('')
+        $('#add-post-form').find('input[name="anonymous"]').val(true);
+    })
+
+    // redirect to signin if current user present and clicks on comment as or navigation
+    $('#switch-post-as').on('click',function(e){
+        if(!localStorage['current_user']){
+            window.location.href = 'signin.html'
+        }
+    })
+
+    $('#btn-send-post').on('click',function(e) {
+        e.preventDefault()
+        content = $('#add-post-form').find('input[name="content"]')
+        if (localStorage['current_user']) {
+            if(content.val() == '') {
+                $('#post-content-text').focus()
+                errorDialog('Error', 'Please enter text')
+            }else if($('input[type=checkbox]:checked').length == 0){
+                errorDialog('Error', 'Please select atleast one tag')
+            }
+            else{
+                // get checked tags
+                var tagsArray = $("#add-post-form input:checkbox:checked").map(function(){
+                    return $(this).val();
+                }).get(); ;
+                var body_params = $.extend({}, $('#add-post-form').serializeHash())
+                body_params['tags_array'] = tagsArray
+                $.ajax({
+                    url: window.api_url + 'posts',
+                    type: 'post',
+                    data: {
+                        'command': "create",
+                        'access_token': localStorage['access_token'],
+                        'body': body_params
+                    },
+                    beforeSend: function () {
+                        showAjaxSpinner();
+                    },
+                    success: function (data) {
+                        hideAjaxSpinner();
+                        sessionStorage.setItem('after-add-post',true)
+                        window.location.href = 'index.html'
+                    },
+                    error: function (xhr, textStatus, errorThrown) {
+                        var error_obj = $.parseJSON(xhr.responseText)
+                        console.log(error_obj)
+                        errorDialog('Error', error_obj.message)
+                        hideAjaxSpinner();
+                    }
+                })
+            }
+
+        }else {
+            window.location.href = 'signin.html'
+        }
+    })
+
 }
